ea78c3b2a3e6e4c099e64b4d924fac9c1fef6247
Vespertino <Vespertino@C2PC24.informatica.comercio>
2021-05-21 11:54:43 +0200

    Segunda version (Refactor)

---
+++ src/Cifrado.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Vespertino
+ */
+public class Cifrado {
+
+    //mÃ©todo para cifrar el texto
+    public static String cifradoCesar(String texto, int posiciones) {
+        StringBuilder cifrado = new StringBuilder();
+        posiciones = posiciones % 26;
+        for (int i = 0; i < texto.length(); i++) {
+            if (texto.charAt(i) >= 'a' && texto.charAt(i) <= 'z') {
+                if ((texto.charAt(i) + posiciones) > 'z') {
+                    cifrado.append((char) (texto.charAt(i) + posiciones - 26));
+                } else {
+                    cifrado.append((char) (texto.charAt(i) + posiciones));
+                }
+            } else if (texto.charAt(i) >= 'A' && texto.charAt(i) <= 'Z') {
+                if ((texto.charAt(i) + posiciones) > 'Z') {
+                    cifrado.append((char) (texto.charAt(i) + posiciones - 26));
+                } else {
+                    cifrado.append((char) (texto.charAt(i) + posiciones));
+                }
+            }
+        }
+        return cifrado.toString();
+    }
+    
+}
--- src/CifradoCesar.java
+++ src/CifradoCesar.java
@@ -1,3 +1,4 @@
+
 import java.io.IOException;
 import java.util.Scanner;
 
@@ -6,7 +7,7 @@ public class CifradoCesar {
     public static void main(String[] args) throws IOException {
         Scanner sc = new Scanner(System.in);
         String texto;
-        int codigo;
+        int posiciones;
         char opcion;
         //Introducir un texto
         do {
@@ -16,62 +17,19 @@ public class CifradoCesar {
         //Introducir el valor del desplazamiento
         do {
             System.out.print("Introduce el cÃ³digo: ");
-            codigo = sc.nextInt();
-        } while (codigo < 1);
+            posiciones = sc.nextInt();
+        } while (posiciones < 1);
         //Introducir la operaciÃ³n a realizar: cifrar o descifrar
         do {
             sc.nextLine();
             System.out.print("(C) cifrar o (D) descifrar?: ");
             opcion = (char) System.in.read();
-        } while (Character.toUpperCase(opcion) != 'C' && Character.toUpperCase(opcion) != 'D');                   
+        } while (Character.toUpperCase(opcion) != 'C' && Character.toUpperCase(opcion) != 'D');
         if (Character.toUpperCase(opcion) == 'C') {
-            System.out.println("Texto cifrado: " + cifradoCesar(texto, codigo));
+            System.out.println("Texto cifrado: " + Cifrado.cifradoCesar(texto, posiciones));
         } else {
-            System.out.println("Texto descifrado: " + descifradoCesar(texto, codigo));
+            System.out.println("Texto descifrado: " + Descifrado.descifradoCesar(texto, posiciones));
         }
     }
 
-    //mÃ©todo para cifrar el texto
-    public static String cifradoCesar(String texto, int codigo) {
-        StringBuilder cifrado = new StringBuilder();
-        codigo = codigo % 26;
-        for (int i = 0; i < texto.length(); i++) {
-            if (texto.charAt(i) >= 'a' && texto.charAt(i) <= 'z') {
-                if ((texto.charAt(i) + codigo) > 'z') {
-                    cifrado.append((char) (texto.charAt(i) + codigo - 26));
-                } else {
-                    cifrado.append((char) (texto.charAt(i) + codigo));
-                }
-            } else if (texto.charAt(i) >= 'A' && texto.charAt(i) <= 'Z') {
-                if ((texto.charAt(i) + codigo) > 'Z') {
-                    cifrado.append((char) (texto.charAt(i) + codigo - 26));
-                } else {
-                    cifrado.append((char) (texto.charAt(i) + codigo));
-                }
-            }
-        }
-        return cifrado.toString();
-    }
-
-    //mÃ©todo para descifrar el texto
-    public static String descifradoCesar(String texto, int codigo) {
-        StringBuilder cifrado = new StringBuilder();
-        codigo = codigo % 26;
-        for (int i = 0; i < texto.length(); i++) {
-            if (texto.charAt(i) >= 'a' && texto.charAt(i) <= 'z') {
-                if ((texto.charAt(i) - codigo) < 'a') {
-                    cifrado.append((char) (texto.charAt(i) - codigo + 26));
-                } else {
-                    cifrado.append((char) (texto.charAt(i) - codigo));
-                }
-            } else if (texto.charAt(i) >= 'A' && texto.charAt(i) <= 'Z') {
-                if ((texto.charAt(i) - codigo) < 'A') {
-                    cifrado.append((char) (texto.charAt(i) - codigo + 26));
-                } else {
-                    cifrado.append((char) (texto.charAt(i) - codigo));
-                }
-            }
-        }
-        return cifrado.toString();
-    }
 } //Fin cifrado Cesar
